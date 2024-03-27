dd55c35e6c390b4a6bf5378c93c51eb45925a1d8
NIKHIL KAMBLE <153905674+nikhilkamble5@users.noreply.github.com>
2024-03-27 14:24:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#"></li>
+          <li href="#"></li>
+          <li href="#"></li>
+          <li href="#"></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
