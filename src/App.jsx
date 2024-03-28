d0f0db4509e33cd745ecf167f0cf049a1f6abf36
NIKHIL KAMBLE <153905674+nikhilkamble5@users.noreply.github.com>
2024-03-28 13:59:55 +0530

    nav bar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Navigation from "./component/Navigation";
+const App = () => {
+  return (
+    <div>
+    <Navigation/>
+    </div>
+  );
+};
+
+export default App;
